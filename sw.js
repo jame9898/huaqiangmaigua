3512701e525191916f87899f95e84962df1dc37f
jame9898 <jame9898@users.noreply.github.com>
2026-06-25 21:43:24 +0800

    init: 敲瓜辨生熟 PWA

---
+++ sw.js
@@ -0,0 +1,28 @@
+var CACHE = "xigua-v1";
+var ASSETS = ["./","./index.html","./manifest.webmanifest","./icon.svg","./icon-192.png","./icon-512.png"];
+
+self.addEventListener("install", function(e){
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ASSETS);}).catch(function(){}));
+});
+
+self.addEventListener("activate", function(e){
+  e.waitUntil(caches.keys().then(function(keys){
+    return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
+  }).then(function(){return self.clients.claim();}));
+});
+
+self.addEventListener("fetch", function(e){
+  var req = e.request;
+  if(req.method !== "GET") return;
+  e.respondWith(
+    caches.match(req).then(function(cached){
+      var net = fetch(req).then(function(res){
+        var copy = res.clone();
+        caches.open(CACHE).then(function(c){c.put(req, copy);}).catch(function(){});
+        return res;
+      }).catch(function(){ return cached; });
+      return cached || net;
+    })
+  );
+});
